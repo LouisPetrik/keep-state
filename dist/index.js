92e133b788250e60c9a36e17dc468485a0cef6ed
Louis <33809479+LouisPetrik@users.noreply.github.com>
2021-09-07 18:56:52 +0200

    rolling up now works

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,23 @@
-const React = require('react')
+'use strict';
+
+var react = require('react');
 
 const useKeepState = (initialState, key) => {
-	const [state, setState] = React.useState(initialState)
+	const [state, setState] = react.useState(initialState);
 
-	React.useLayoutEffect(() => {
+	react.useLayoutEffect(() => {
 		if (sessionStorage.getItem(key)) {
-			setState(JSON.parse(sessionStorage.getItem(key)))
+			setState(JSON.parse(sessionStorage.getItem(key)));
 		} else {
-			sessionStorage.setItem(key, JSON.stringify(state))
+			sessionStorage.setItem(key, JSON.stringify(state));
 		}
-	}, [])
+	}, []);
 
-	React.useEffect(() => {
-		sessionStorage.setItem(key, JSON.stringify(state))
-	}, [state])
+	react.useEffect(() => {
+		sessionStorage.setItem(key, JSON.stringify(state));
+	}, [state]);
 
 	return [state, setState]
-}
-module.exports = useKeepState
+};
+
+module.exports = useKeepState;
