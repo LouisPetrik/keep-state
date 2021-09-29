fe0bc219a6a35b9beed92ffa15de45dea1e7ade9
Louis <33809479+LouisPetrik@users.noreply.github.com>
2021-09-29 20:05:16 +0200

    new indentation

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 import { useEffect, useState, useLayoutEffect } from 'react'
 
 const useKeepState = (initialState, key) => {
-	// SSR is true, if the window object is not defined
-	const SSR = typeof window === 'undefined'
+  // SSR is true, if the window object is not defined
+  const SSR = typeof window === 'undefined'
 
-	const [state, setState] = useState(initialState)
+  const [state, setState] = useState(initialState)
 
-	// to make sure in Next.js, the rendering is only happening
-	// client-side. Else, next will complain and idk why.
+  // to make sure in Next.js, the rendering is only happening
+  // client-side. Else, next will complain and idk why.
+  if (!SSR) {
+    useLayoutEffect(() => {
+      if (sessionStorage.getItem(key)) {
+        setState(JSON.parse(sessionStorage.getItem(key)))
+      } else {
+        sessionStorage.setItem(key, JSON.stringify(state))
+      }
+    }, [])
+  }
 
-	useLayoutEffect(() => {
-		if (sessionStorage.getItem(key)) {
-			setState(JSON.parse(sessionStorage.getItem(key)))
-		} else {
-			sessionStorage.setItem(key, JSON.stringify(state))
-		}
-	}, [])
+  useEffect(() => {
+    sessionStorage.setItem(key, JSON.stringify(state))
+  }, [state])
 
-	useEffect(() => {
-		sessionStorage.setItem(key, JSON.stringify(state))
-	}, [state])
-
-	return [state, setState]
-	//return !SSR ? [state, setState] : null
+  return [state, setState]
+  //return !SSR ? [state, setState] : null
 }
 export default useKeepState
